dc935d7207494f421cf4f76c2ec87d74be96441a
Tiffany Pohl <pohltm@rose-hulman.edu>
2012-03-26 21:17:49 -0400

    test ten passed

--- Eclipse/PrimeNumberGenerator/src/PrimeNumberGenerator.java
+++ Eclipse/PrimeNumberGenerator/src/PrimeNumberGenerator.java
@@ -6,19 +6,23 @@ public class PrimeNumberGenerator {
 	public static ArrayList<Integer> generatePrimes(int n){
 		ArrayList<Integer> ret =  new ArrayList<Integer>();
 		
-		for (int i = 2; i < n; i++){
+		for (int i = 0; i < n; i++){
 			ret.add(i);
 		}
 		
-		for(int i = 2; i < n; i+=2){
-			if (i < ret.size()){
-				ret.set(i, 0);
+		for (int j = 2; j < n; j++){
+//			int p = ret.get(j);
+			for(int i = 2*j; i < n; i+=j){
+				if (i < ret.size()){
+					ret.set(i, 0);
+				}
 			}
 		}
 		
 		while(ret.contains(0)){
 			ret.remove(new Integer(0));
 		}
+		ret.remove(new Integer(1));
 		
 		return ret;
 	}
