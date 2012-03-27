c69cc7820025d347ac6c9a9d2e2c37fcec321621
Tiffany Pohl <pohltm@rose-hulman.edu>
2012-03-26 21:21:56 -0400

    test OneHundredFifty added, and passed. I think we are done.

--- Eclipse/PrimeNumberGenerator/src/PrimeNumberGeneratorTest.java
+++ Eclipse/PrimeNumberGenerator/src/PrimeNumberGeneratorTest.java
@@ -47,6 +47,11 @@ public class PrimeNumberGeneratorTest {
 		assertEquals(list(2, 3, 5, 7, 11, 13, 17, 19, 23), PrimeNumberGenerator.generatePrimes(24));
 	}
 	
+	@Test
+	public void testOneHundredFifty(){
+		assertEquals(list(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149), PrimeNumberGenerator.generatePrimes(150));
+	}
+	
 	private ArrayList<Integer> list(int... ints) {
 		ArrayList<Integer> ret = new ArrayList<Integer>();
 		for(int i : ints) {
