6cfeb38994a5c77a293fe7c819d7e76d444b4a1a
Tiffany Pohl <pohltm@rose-hulman.edu>
2012-03-26 20:28:40 -0400

    second test added

--- Eclipse/PrimeFactorization/src/PrimeFactorizationTest.java
+++ Eclipse/PrimeFactorization/src/PrimeFactorizationTest.java
@@ -9,6 +9,19 @@ import org.junit.Test;
 public class PrimeFactorizationTest {
 	@Test
 	public void testOne() {
-		assertEquals(new ArrayList<Integer>(), PrimeFactorization.generateFactors(1));
+		assertEquals(list(), PrimeFactorization.generateFactors(1));
+	}
+	
+	@Test
+	public void testTwo() {
+		assertEquals(list(2), PrimeFactorization.generateFactors(2));
+	}
+	
+	private ArrayList<Integer> list(int... ints) {
+		ArrayList<Integer> ret = new ArrayList<Integer>();
+		for(int i : ints) {
+			ret.add(i);
+		}
+		return ret;
 	}
 }
