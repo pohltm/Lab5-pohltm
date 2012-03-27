82a9f6c8578c5fa49c0c002d3c6f62846329b5a4
Tiffany Pohl <pohltm@rose-hulman.edu>
2012-03-26 21:10:43 -0400

    test five passed

--- Eclipse/PrimeNumberGenerator/src/PrimeNumberGenerator.java
+++ Eclipse/PrimeNumberGenerator/src/PrimeNumberGenerator.java
@@ -10,6 +10,16 @@ public class PrimeNumberGenerator {
 			ret.add(i);
 		}
 		
+		for(int i = 2; i < n; i+=2){
+			if (i < ret.size()){
+				ret.set(i, 0);
+			}
+		}
+		
+		while(ret.contains(0)){
+			ret.remove(new Integer(0));
+		}
+		
 		return ret;
 	}
 	
