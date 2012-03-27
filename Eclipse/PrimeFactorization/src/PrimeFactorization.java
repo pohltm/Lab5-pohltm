756d95c12cfad1120ed774dff815f38beb303847
Tiffany Pohl <pohltm@rose-hulman.edu>
2012-03-26 20:34:46 -0400

    fourth test passed

--- Eclipse/PrimeFactorization/src/PrimeFactorization.java
+++ Eclipse/PrimeFactorization/src/PrimeFactorization.java
@@ -6,6 +6,11 @@ public class PrimeFactorization {
 	public static ArrayList<Integer> generateFactors(int i) {
 		ArrayList<Integer> ret = new ArrayList<Integer>();
 		
+		if (i % 2 == 0) {
+			ret.add(2);
+			i /= 2;
+		}
+		
 		if (i > 1) {
 			ret.add(i);
 		}
